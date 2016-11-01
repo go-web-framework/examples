85af3d9fcbb50fc24fa87d96dcb44e83d891abb2
rahuljaisimha <rahul@jaisimha.com>
2016-10-31 21:31:54 -0500

    Finished the api example

---
+++ api_example.go
@@ -0,0 +1,41 @@
+// gflux/api allows programmers to create a REST API easily.
+// FUTURE FEATURE : This tool automatically creates html pages to get and set
+
+package main
+
+import (
+	"fmt"
+	"gflux/api"
+	"net/http"
+)
+
+type Post struct {
+	api.Model
+	Title string
+	Author string
+}
+
+type User struct {
+	api.Model
+	Name string
+}
+
+func main(){
+	// create new REST API that uses sqlite3 database database.db as the datastore
+	a := api.New("sqlite3", "database.db")
+	
+	// create a new resource posts which is a new table in database.db
+	rposts := api.NewResource("posts", &Post{})
+	
+	// create resource users which allows GET and POST requests
+	rusers := api.NewResource("users", &User{}).allow("GET", "POST")
+	
+	// OPTIONAL: override GET handler. Same can be done for POST handler.
+	// Default handler only handles application/json and application/xml
+	rusers.Handlers["GET"] = func(user User, w http.responseWriter, accepts []string){
+		// IMPLEMENTATION that writes to w. accepts looks like ["html", "json"]
+	}
+	
+	a.Serve()
+}
+	
