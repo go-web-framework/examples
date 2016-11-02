e8a756130a683a6737134b1edde045a674d0d0bd
taiyio <taiyi.o@gmail.com>
2016-11-01 17:54:32 -0700

    tuesday example

--- blog_example/goblog.go
+++ blog_example/goblog.go
@@ -3,27 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"html/template"
+	//"html/template"
 	"github.com/jinzhu/gorm"
+	//_ "github.com/go-sql-driver/mysql"
+	"strconv"
+	"github.com/go-web-framework/gflux/mux"
+	//"io/ioutil"
+	//"./mux"
+	//"github.com/google/uuid"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-//	"strconv"
-//	"github.com/go-web-framework/gflux/mux"
-	"./mux"
-	"github.com/google/uuid"
+	//"encoding/json"
+	"github.com/go-web-framework/templates"
+	//"path/filepath"
 )
 
 var db *gorm.DB
-
-var templates = template.Must(template.ParseFiles("goblog.html", "page.html"))
+var userName string
+var userID uint
+//var templates = template.Must(template.ParseFiles("goblog.html", "page.html"))
+var s = templates.Set{}
 
 // table posts (
-//   Post_id: varchar(30)
+//   Post_id: int (autoincrement)
 //   Author: varchar(30)
 //   Aext: varchar(200)
 // )
 func main(){
 
-	// open database
 	var err error
 	db, err = gorm.Open("sqlite3", "goblog.db")
 	if err != nil {
@@ -32,31 +38,98 @@ func main(){
 	defer db.Close()
 	
 	// Migrate the schema
+	//db.CreateTable(&User{})
  	db.AutoMigrate(&Post{})
- 	
+ 	db.AutoMigrate(&User{})
+ 	db.Delete(Post{})
+	db.Delete(User{})
+	
+	//create default user
+	userName = "default"
+	var user = User{Name: userName}
+	db.Create(&user)
+	var user2 User
+	db.Where("Name= ?", userName).First(&user2)
+	userID = user2.ID
+	var post = Post{UserID: userID ,Author: userName, Text: "hello"}
+	db.Create(&post)
+	fmt.Printf("%d\n", post.Model.ID)
+ 
+	s.Parse("templates")
+
 	testMux := mux.New()
 	homeHandler := homeHandler{}
 	pageHandler := pageHandler{}
 	newHandler := newHandler{}
+	userHandler := userHandler{}
+	newUserHandler := newUserHandler{}
+	changeUserHandler := changeUserHandler{}
 	testMux.GET("/home", nil, homeHandler)
 	testMux.GET("/page/{id}", nil, pageHandler)
 	testMux.POST("/page/new", nil, newHandler)
+	testMux.GET("/newuser", nil, userHandler)
+	testMux.POST("/makeNewUser", nil, newUserHandler)
+	testMux.POST("/changeUser", nil, changeUserHandler)
 	fmt.Println("Listening on :8080")
 	http.ListenAndServe(":8080", testMux)
 	
 }
+type changeUserHandler struct{}
+func (t changeUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	name := r.FormValue("Name")
+	var user User
+	db.Where("Name= ?", name).First(&user)
+	userName = user.Name
+	userID = user.ID
+	http.Redirect(w, r, "/home", http.StatusFound)
+}
+type userHandler struct{}
+func (t userHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "newuser.html")
+}
 
+type newUserHandler struct{}
+func (t newUserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	name := r.FormValue("Name")
+	//no name
+	//unique name
+	if (name == ""){
+		http.Redirect(w, r, "/newUser", http.StatusFound)
+	}
+	//store user
+	var userList []User
+	db.Find(&userList)
+	var user = User{Name:name}
+	db.Create(&user)
+	
+	http.Redirect(w, r, "/home", http.StatusFound)
+}
+
+type jsHandler struct{}
+func (t jsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	http.ServeFile(w, r, "posts.js");
+}
 type homeHandler struct{
 }
 
 func (t homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	//make database call
+	
 	var postList []Post
-	db.Find(&postList)
-	err := templates.ExecuteTemplate(w, "goblog.html", &goBlog{PostList: postList})
+	var user User
+	db.Where("Name = ?", userName).First(&user)
+	//fmt.Println("name %s\n", user.Name)
+	//db.Find(&postList)
+	db.Model(&user).Related(&postList)
+	//err := templates.ExecuteTemplate(w, "goblog.html", &goBlog{Username: userName, PostList: postList})
+	m := make(map[string]interface{})
+	m["Username"] = userName
+	m["PostList"] = postList
+	err := s.Execute("goblog.html", w, m)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
+	
 	return
 }
 
@@ -64,11 +137,25 @@ type pageHandler struct{
 }
 
 func (t pageHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	//database call
+	/*url := r.URL.Path
+	urls := strings.Split(url, "/")
+	idURL, err := strconv.Atoi(urls[len(urls)-1])
+	if (err != nil){
+		idURL = 0
+	}*/
 	params := mux.GetParams(r)
-	idURL := params["id"]
+	idURL, err := strconv.Atoi(params["id"])
+	if (err != nil){
+		idURL = 0
+	}
 	var post Post
-	db.Where("Post_id = ?", idURL).First(&post)
-	err := templates.ExecuteTemplate(w, "page.html", &post)
+	db.Where("ID = ?", idURL).First(&post)
+	m := make(map[string]interface{})
+	m["Author"] = post.Author
+	m["Text"] = post.Text
+	//err = templates.ExecuteTemplate(w, "page.html", &post)
+	err = s.Execute("page.html", w, m)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
     }
@@ -79,16 +166,19 @@ type newHandler struct{
 }
 
 func (t newHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
-	author := r.FormValue("Author")
+	author := r.FormValue("author")
 	if (author == ""){
 		author = "anon"
 	}
-	text := r.FormValue("Text")
+	text := r.FormValue("text")
+	
+	//store post
+	var post = Post{UserID: userID, Author:author, Text:text}
+	db.Create(&post)
+	//send as json
 	var postList []Post
 	db.Find(&postList)
-	//store post
-	var post = Post{Author:author, Text:text, Post_id:uuid.New().String()}
-	db.Create(&post)
+	
 	
 	http.Redirect(w, r, "/home", http.StatusFound)
 }
@@ -96,14 +186,24 @@ func (t newHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 
 //ajax
 type goBlog struct{
+	Username string
 	PostList []Post
 }
 
 type Post struct{
 	gorm.Model
-	Post_id 	string 	`gorm:""primary_key"`
-	Author 		string `gorm:"type:varchar(20)"`
-	Text 			string	`gorm:"type:varchar(200)"`
+	User User			`gorm:"ForeignKey:UserID"`
+	UserID 		uint 	`json:"userid"`
+	//Post_id 	int 	`gorm:""primary_key"`
+	Author 		string `gorm:"type:varchar(20)" json:"author"`
+	Text 			string	`gorm:"type:varchar(200)" json:"text"`
+}
+
+type User struct{
+	gorm.Model
+	Posts []Post
+	//User_id 	int `gorm:"primary_key"`
+	Name		string `gorm:"type:varchar(20)"`
 }
 
 type handler404 struct{
