2ec5642fc9d5c310efb728baf7d4ef30d904cc26
Joshua <jdong42@gmail.com>
2016-11-07 14:32:11 -0600

    fix compilation error

--- api_example.go
+++ api_example.go
@@ -4,38 +4,38 @@
 package main
 
 import (
-	"fmt"
-	"github.com/go-web-framework/gflux/api"
-	"net/http"
+    //"fmt"
+    "github.com/go-web-framework/gflux/api"
+    "net/http"
 )
 
 type Post struct {
-	api.Model
-	Title string
-	Author string
+    api.Model
+    Title string
+    Author string
 }
 
 type User struct {
-	api.Model
-	Name string
+    api.Model
+    Name string
 }
 
 func main(){
-	// create new REST API that uses sqlite3 database database.db as the datastore
-	a := api.New("sqlite3", "database.db")
-	
-	// create a new resource posts which is a new table in database.db
-	rposts := api.NewResource("posts", &Post{})
-	
-	// create resource users which allows GET and POST requests
-	rusers := api.NewResource("users", &User{}).allow("GET", "POST")
-	
-	// OPTIONAL: override GET handler. Same can be done for POST handler.
-	// Default handler only handles application/json and application/xml
-	rusers.Handlers["GET"] = func(user User, w http.responseWriter, accepts []string){
-		// IMPLEMENTATION that writes to w. accepts looks like ["html", "json"]
-	}
-	
-	a.Serve()
+    // create new REST API that uses sqlite3 database database.db as the datastore
+    a := api.New("sqlite3", "database.db")
+
+    // create a new resource posts which is a new table in database.db
+    Rposts := api.NewResource("posts", &Post{})
+
+    // create resource users which allows GET and POST requests
+    Rusers := api.NewResource("users", &User{}).allow("GET", "POST")
+
+    // OPTIONAL: override GET handler. Same can be done for POST handler.
+    // Default handler only handles application/json and application/xml
+    Rusers.Handlers["GET"] = func(user User, w http.responseWriter, accepts []string){
+        // IMPLEMENTATION that writes to w. accepts looks like ["html", "json"]
+    }
+
+    a.Serve()
 }
-	
+
